fa2070e1a12c1acaa6d3fa149d6a7f3b01b5d024
Красиков Иван <krasikov@intaro.email>
2025-03-10 14:44:51 +0300

    feat: доделал rsa

--- lr/lr3.go
+++ lr/lr3.go
@@ -1,17 +1,66 @@
 package lr
 
-import "github.com/cipher/rsa"
+import (
+	"github.com/cipher/rsa"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
 
 func Lr3() {
-	//fmt.Println("LR3")
-	//enKey, decKey, err := rsa.GenerateKeys(16)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//num := 33
-	//fmt.Println("Исходное число:", num)
-	//cip := rsa.Code(33, enKey)
-	//fmt.Println("Зашифрованное число:", cip.Text(10))
-	//fmt.Println("Расшифрованное число:", rsa.Decode(cip, decKey))
-	rsa.Test()
+	router := gin.Default()
+	router.Use(CORSMiddleware())
+	router.GET("/api/generate-keys", func(ctx *gin.Context) {
+		enKey, deKey, err := rsa.GenerateKeys(1024)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"publicKey":  enKey,
+			"privateKey": deKey,
+		})
+	})
+	router.POST("/api/encrypt", func(ctx *gin.Context) {
+		type Request struct {
+			Text      string `json:"text"`
+			PublicKey string `json:"publicKey"`
+		}
+		var request Request
+		if err := ctx.ShouldBind(&request); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		result, err := rsa.Encrypt(request.Text, request.PublicKey)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"encryptedText": result,
+		})
+	})
+	router.POST("/api/decrypt", func(ctx *gin.Context) {
+		type Request struct {
+			Text       string `json:"text"`
+			PrivateKey string `json:"privateKey"`
+		}
+		var request Request
+		if err := ctx.ShouldBind(&request); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		result, err := rsa.Decrypt(request.Text, request.PrivateKey)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"decryptedText": result,
+		})
+	})
+	err := router.Run(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- rsa/rsa.go
+++ rsa/rsa.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"math/big"
+	"strings"
 )
 
 type Key struct {
@@ -12,12 +13,44 @@ type Key struct {
 	N  *big.Int
 }
 
+func Encrypt(word string, key string) (string, error) {
+	enKey, err := HexToKey(key)
+	if err != nil {
+		return "", err
+	}
+	arrayInt := WordToArrayInt(word)
+	result := make([]string, len(arrayInt))
+	for i, item := range arrayInt {
+		result[i] = IntToHex(Code(item, enKey))
+	}
+	return strings.Join(result, "/"), nil
+}
+
+func Decrypt(word string, key string) (string, error) {
+	deKey, err := HexToKey(key)
+	if err != nil {
+		return "", err
+	}
+	arrayInt, err := HexStringToArrayInt(word)
+	if err != nil {
+		return "", err
+	}
+	resultDecode := make([]*big.Int, len(arrayInt))
+	for i, item := range arrayInt {
+		resultDecode[i] = Decode(item, deKey)
+	}
+	result := make([]byte, len(resultDecode))
+	for i, item := range resultDecode {
+		result[i] = byte(item.Int64())
+	}
+	return string(result), nil
+}
+
 func Test() {
 	fmt.Println(searchD(big.NewInt(17), big.NewInt(2651394840)).Text(10))
 }
 
-func Code(number int, key *Key) *big.Int {
-	num := big.NewInt(int64(number))
+func Code(num *big.Int, key *Key) *big.Int {
 	cip := num.Exp(num, key.Kn, nil)
 	cip.Mod(cip, key.N)
 	return cip
@@ -27,39 +60,34 @@ func Decode(number *big.Int, key *Key) *big.Int {
 	return number.Exp(number, key.Kn, key.N)
 }
 
-func GenerateKeys(bits int) (*Key, *Key, error) {
+func GenerateKeys(bits int) (string, string, error) {
 	p, err := generatePrime(bits)
 	if err != nil {
-		return nil, nil, err
+		return "", "", err
 	}
 	q, err := generatePrime(bits)
 	if err != nil {
-		return nil, nil, err
+		return "", "", err
 	}
 	if p.Text(10) == q.Text(10) {
 		p, err = generatePrime(bits)
 		if err != nil {
-			return nil, nil, err
+			return "", "", err
 		}
 	}
-	fmt.Println("P и Q:", p.Text(10), q.Text(10))
 	n := big.NewInt(1)
 	n = n.Mul(p, q)
-	fmt.Println("N:", n.Text(10))
 	subP := big.NewInt(1).Sub(p, big.NewInt(1))
 	subQ := big.NewInt(1).Sub(q, big.NewInt(1))
 	fn := big.NewInt(1).Mul(subP, subQ)
-	fmt.Println("Fn:", fn.Text(10))
 	e := searchE(fn)
 	if e == nil {
-		return nil, nil, errors.New("error generate e")
+		return "", "", errors.New("error generate e")
 	}
-	fmt.Println("E:", e.Text(10))
 	d := searchD(e, fn)
 	if d == nil {
-		return nil, nil, errors.New("error generate d")
+		return "", "", errors.New("error generate d")
 	}
-	fmt.Println("D:", d.Text(10))
 	keyEnc := &Key{
 		Kn: e,
 		N:  n,
@@ -68,7 +96,7 @@ func GenerateKeys(bits int) (*Key, *Key, error) {
 		Kn: d,
 		N:  n,
 	}
-	return keyEnc, keyDec, nil
+	return KeyToHex(keyEnc), KeyToHex(keyDec), nil
 }
 
 func generatePrime(bits int) (*big.Int, error) {
@@ -98,4 +126,67 @@ func searchE(fn *big.Int) *big.Int {
 
 func searchD(e, fn *big.Int) *big.Int {
 	return big.NewInt(1).ModInverse(e, fn)
+}
+
+func IntToHex(num *big.Int) string {
+	return num.Text(16)
+}
+
+func HexToInt(hex string) (*big.Int, error) {
+	result, ok := big.NewInt(0).SetString(hex, 16)
+	if !ok {
+		return nil, errors.New("error convert hex to int")
+	}
+	return result, nil
+}
+
+func ByteToInt(char byte) *big.Int {
+	return big.NewInt(int64(char))
+}
+
+func WordToArrayInt(word string) []*big.Int {
+	wordChars := []byte(word)
+	result := make([]*big.Int, len(wordChars))
+	for i, char := range wordChars {
+		result[i] = ByteToInt(char)
+	}
+	return result
+}
+
+func HexStringToArrayInt(cip string) ([]*big.Int, error) {
+	hexArray := strings.Split(cip, "/")
+	result := make([]*big.Int, len(hexArray))
+	for i, hex := range hexArray {
+		bint, err := HexToInt(hex)
+		if err != nil {
+			return nil, err
+		}
+		result[i] = bint
+	}
+	return result, nil
+}
+
+func KeyToHex(key *Key) string {
+	kn := IntToHex(key.Kn)
+	n := IntToHex(key.N)
+	return strings.Join([]string{kn, n}, ":")
+}
+
+func HexToKey(hex string) (*Key, error) {
+	arr := strings.Split(hex, ":")
+	if len(arr) != 2 {
+		return nil, errors.New("error convert hex to key")
+	}
+	kn, err := HexToInt(arr[0])
+	if err != nil {
+		return nil, err
+	}
+	n, err := HexToInt(arr[1])
+	if err != nil {
+		return nil, err
+	}
+	return &Key{
+		Kn: kn,
+		N:  n,
+	}, nil
 }
